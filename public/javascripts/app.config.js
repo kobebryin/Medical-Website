8a820d3e1360cd0aac6a891f3bc783fbbf069470
DESKTOP-3PO0804\Jimmy Liang <kobebryin@gmail.com>
2018-03-29 00:09:04 +0800

    2018.03.29 網頁前端整合完成

--- public/javascripts/app.config.js
+++ public/javascripts/app.config.js
@@ -9,5 +9,51 @@ angular.module('Medical Website').config(function($stateProvider, $urlRouterProv
       url: "/",
       templateUrl: "views/index.html"/*,
       controller: "IndexController"*/
+    })
+    .state('write_caseinfo', {
+      url: "/write_caseinfo",
+      templateUrl: "views/write_caseinfo.html"/*,
+      controller: "IndexController"*/
+    })
+    .state('read_caseinfo', {
+      url: "/read_caseinfo",
+      templateUrl: "views/read_caseinfo.html"/*,
+      controller: "IndexController"*/
+    })
+    .state('write_casedata', {
+      url: "/write_casedata",
+      templateUrl: "views/write_casedata.html"/*,
+      controller: "IndexController"*/
+    })
+    .state('read_casedata', {
+      url: "/read_casedata",
+      templateUrl: "views/read_casedata.html"/*,
+      controller: "IndexController"*/
+    })
+    .state('write_careinfo', {
+      url: "/write_careinfo",
+      templateUrl: "views/write_careinfo.html"/*,
+      controller: "IndexController"*/
+    })
+    .state('read_careinfo', {
+      url: "/read_careinfo",
+      templateUrl: "views/read_careinfo.html"/*,
+      controller: "IndexController"*/
+    })
+    .state('read_daycareinfo', {
+      url: "/read_daycareinfo",
+      templateUrl: "views/read_daycareinfo.html"/*,
+      controller: "IndexController"*/
+    })
+    .state('write_endinfo', {
+      url: "/write_endinfo",
+      templateUrl: "views/write_endinfo.html"/*,
+      controller: "IndexController"*/
+    })
+    .state('read_endinfo', {
+      url: "/read_endinfo",
+      templateUrl: "views/read_endinfo.html"/*,
+      controller: "IndexController"*/
     });
+
 });
