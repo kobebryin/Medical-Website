4bf90661d58d4fd5d8cea182cb82f98127c6c83f
Jimmy <kobebryin@gmail.com>
2018-03-29 19:16:56 +0800

    2018.03.29 將indexController的BBS Service拔掉避免報錯

--- public/javascripts/controllers/indexController.js
+++ public/javascripts/controllers/indexController.js
@@ -1,4 +1,4 @@
-angular.module('Medical Website').controller('IndexController', function ($rootScope, $scope, BbsService) {
+angular.module('Medical Website').controller('IndexController', function ($rootScope, $scope) {
     $scope.content = 'Web Developer : Jimmy Liang \n Date: 2017.09.30';
 
     
