51ccc38bc7d84e4afda575041c6f79c1ee803a77
DESKTOP-3PO0804\Jimmy Liang <kobebryin@gmail.com>
2018-05-11 02:09:58 +0800

    2018.05.11 port號改為319

--- public/javascripts/app.run.js
+++ public/javascripts/app.run.js
@@ -3,5 +3,5 @@ angular.module('Medical Website').run(function($rootScope){
     * Author : Jimmy Liang
     * Date : 2018/03/28
     */
-    $rootScope.apiUrl = 'http://127.0.0.1:8080/';
+    $rootScope.apiUrl = 'http://127.0.0.1:319/';
 });
